16dcd446e6f40c8105cd34e97f430961d2e1d407
fergmac <f.macconnell@gmail.com>
2017-04-26 16:21:06 -0700

    change App from functional component to class with api search

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar';
 
 const API_KEY = 'AIzaSyCuuLsWCe3__BSsGMlNUepwyBeGZSVpy1s';
 
 // Create new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+        // Use Youtube api to searh for videos with a term of 'surfboards'
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) =>{
+             this.setState({ videos });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 // Take this components generated HTML and put it on the page( in the DOM)
